960eea3ec743c6daea32f9e7d0da530fd6be9045
cipherswan <leskauskas25@gmail.com>
2017-12-12 20:59:33 +0200

    Add CORS

--- src/main/java/lt/java/uzduotys/uzduotishelpme/Application.java
+++ src/main/java/lt/java/uzduotys/uzduotishelpme/Application.java
@@ -2,6 +2,10 @@ package lt.java.uzduotys.uzduotishelpme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.persistence.EntityManager;
 
@@ -11,4 +15,15 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:9000");
+			}
+		};
+	}
 }
+
