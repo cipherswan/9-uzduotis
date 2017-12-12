14ae165120ce860dd00f76405f98d44ef147822f
cipherswan <leskauskas25@gmail.com>
2017-12-12 19:47:49 +0200

    Add Entities & SQL queries

--- src/main/java/lt/java/uzduotys/uzduotishelpme/Application.java
+++ src/main/java/lt/java/uzduotys/uzduotishelpme/Application.java
@@ -7,7 +7,6 @@ import javax.persistence.EntityManager;
 
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
