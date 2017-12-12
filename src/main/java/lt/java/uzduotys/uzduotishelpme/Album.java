f2d5bf1b7d867fb4611bcbe1d636ce81b0836a1d
cipherswan <leskauskas25@gmail.com>
2017-12-12 19:28:25 +0200

    Add data.sql

--- src/main/java/lt/java/uzduotys/uzduotishelpme/Album.java
+++ src/main/java/lt/java/uzduotys/uzduotishelpme/Album.java
@@ -10,6 +10,7 @@ public class Album {
     private Long id;
     private String albumName;
 
+
     public Album(){}
 
     public Album(String albumName) {
--- src/main/java/lt/java/uzduotys/uzduotishelpme/Application.java
+++ src/main/java/lt/java/uzduotys/uzduotishelpme/Application.java
@@ -3,6 +3,8 @@ package lt.java.uzduotys.uzduotishelpme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.persistence.EntityManager;
+
 @SpringBootApplication
 public class Application {
 
